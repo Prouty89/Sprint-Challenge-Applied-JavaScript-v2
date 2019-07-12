b0df938aed15b385579a4befd8f29c3154afdaba
Blake <bpro0821@gmail.com>
2019-07-12 13:32:02 -0500

    having issues with header

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,38 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
+const headerContainer = document.getElementsByClassName('.headerContainer').appendChild(headerContainer)
 
-function Header() {}
+//headerContainer.appendChild(headerCont)
+// data.forEach( (date, title, temp) => {
+//     let createHeader = buttonCreator(arrayItem);
+
+//     parent.appendChild(newButton);
+// }}
+
+function createHeader() {
+
+    //create elements
+  const header = document.createElement('div')
+  const date = document.createElement('span')
+  const title = document.createElement('h1')
+  const temp = document.createElement('span')
+
+    //class names
+  header.classList.add('header')
+  date.classList.add('span')
+  title.classList.add('h1')
+  temp.classList.add('temp')
+
+    //content
+  date.textContent = 'MARCH 28';
+  title.textContent = 'Lambda Times';
+  temp.textContent = '98';
+
+  header.appendChild(date)
+  header.appendChild(title)
+  header.appendChild(temp)
+
+  return header
+}
